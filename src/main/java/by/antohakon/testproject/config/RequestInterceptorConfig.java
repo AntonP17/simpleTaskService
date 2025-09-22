5d7128f652fd074bd413bbdc2c2a91b2d6d246c8
AntonP17 <pokrovskii.anton@mail.ru>
2025-09-22 21:45:34 +0300

    add path

--- src/main/java/by/antohakon/testproject/config/RequestInterceptorConfig.java
+++ src/main/java/by/antohakon/testproject/config/RequestInterceptorConfig.java
@@ -1,16 +1,22 @@
 package by.antohakon.testproject.config;
 
 import by.antohakon.testproject.interceptors.RequestInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class RequestInterceptorConfig  implements WebMvcConfigurer {
+public class RequestInterceptorConfig implements WebMvcConfigurer {
+
+    @Autowired
+    RequestInterceptor requestInterceptor;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new RequestInterceptor());
+        System.out.println("method config.addInterceptor after inject bean");
+        registry.addInterceptor(requestInterceptor)
+                .addPathPatterns("/api/*");
     }
 
 }
