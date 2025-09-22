5b3d2777b21af11c295c9163fffca74cf32b8898
AntonP17 <pokrovskii.anton@mail.ru>
2025-09-22 21:36:30 +0300

    add interceptors

---
+++ src/main/java/by/antohakon/testproject/config/RequestInterceptorConfig.java
@@ -0,0 +1,16 @@
+package by.antohakon.testproject.config;
+
+import by.antohakon.testproject.interceptors.RequestInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class RequestInterceptorConfig  implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new RequestInterceptor());
+    }
+
+}
